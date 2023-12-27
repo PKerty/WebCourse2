204e51f3cf85d61406ddeaafcf338e3e0ae8ae82
PKerty <pcurti61@gmail.com>
2023-12-27 20:56:07 -0300

    finished with fundMe

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -9,15 +9,15 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 
-contracts FundFundMe is Script {
+contract FundFundMe is Script {
 
-    uint256 constant SEND_VALUE = 0.01 ether;
+    uint256 constant SEND_VALUE = 0.1 ether;
     function run() external {
         address deployAddress = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
         fundFundMe(deployAddress);
     }   
-    function FundFundMe(address mostRecentDeploy) public {
-        vm.startBroadcast()
+    function fundFundMe(address mostRecentDeploy) public {
+        vm.startBroadcast();
         FundMe(payable(mostRecentDeploy)).fund{
             value: SEND_VALUE
         }();
@@ -25,6 +25,17 @@ contracts FundFundMe is Script {
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 }
-contracts WithdrawFundMe is Script {
-    
+contract WithdrawFundMe is Script {
+
+    function run() external {
+        address deployAddress = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        withdrawFundMe(deployAddress);
+    }   
+    function withdrawFundMe(address mostRecentDeploy) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentDeploy)).withdraw();
+        vm.stopBroadcast();
+    }
+
 }
+
